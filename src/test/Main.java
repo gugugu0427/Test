026722836b94b2341cead594f469556e7f6ccb36
guyang <guy@startimes.com.cn>
2017-08-30 19:59:44 +0800

    1111

--- src/test/Main.java
+++ src/test/Main.java
@@ -7,4 +7,7 @@ public class Main {
     public static void main(String args[]){
         System.out.println("你好,gitHub111acccccc");
     }
+    public void test(){
+        System.out.println("11111");
+    }
 }
